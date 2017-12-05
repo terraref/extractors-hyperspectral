b8a309091e46bf14c17a1838f981731af663b604
Max Burnette <mburnet2@illinois.com>
2017-12-05 12:56:40 -0600

    add batch stuff

---
+++ hyperspectral/batch_scratch1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# this script is called to invoke one instance of hyperspectral extractor.
+
+# Load necessary modules
+module purge
+module load python/2.7.10 pythonlibs/2.7.10 gdal-stack-2.7.10 nco
+
+# Activate python virtualenv
+source /projects/arpae/terraref/shared/extractors/pyenv/bin/activate
+
+# Run extractor script
+python /projects/arpae/terraref/shared/extractors/extractors-hyperspectral/hyperspectral/hyperspectral_scratch.py --input="/scratch/vnir_netcdf/vnir_pt1.txt"
