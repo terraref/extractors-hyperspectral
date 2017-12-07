2be742248d61aef52de1b0a2aeb1fe265882392c
Max Burnette <mburnet2@illinois.com>
2017-12-07 09:04:10 -0600

    improvements

--- hyperspectral/hyperspectral_envlog.py
+++ hyperspectral/hyperspectral_envlog.py
@@ -1,14 +1,21 @@
 import os
 import subprocess
 
+vnir_list = "/gpfs_scratch/vnir_netcdf/vnir_dates.txt"
 env_log = "/projects/arpae/terraref/sites/ua-mac/Level_1/envlog_netcdf"
 alt_out = "/gpfs_scratch/envlog_netcdf"
 
-for date in os.listdir(env_log):
+vnir_dates = []
+with open(vnir_list, 'r') as inp:
+    for date in inp:
+        vnir_dates.append(date.rstrip())
+
+# Prioritize dates w/ VNIR first, then the rest
+for date in vnir_dates: #os.listdir(env_log)
     date_dir = os.path.join(env_log, date)
 
     allfiles = os.listdir(date_dir)
-    ncfiles = [f for f in allfiles if f.endswith(".nc")]
+    ncfiles = [os.path.join(date_dir, f) for f in allfiles if f.endswith(".nc")]
     ncfiles.sort()
 
     out_file = "envlog_netcdf_L1_ua-mac_%s.nc" % date
@@ -16,6 +23,7 @@ for date in os.listdir(env_log):
     if not os.path.isdir(out_path):
         os.makedirs(out_path)
 
-    print("concatenating %s files from %s" % (len(ncfiles), date))
-    cmd = "ncrcat "+" ".join(ncfiles)+" "+os.path.join(out_path, out_file)
-    subprocess.call([cmd], shell=True)
+    if not os.path.isfile(os.path.join(out_path, out_file)):
+        print("concatenating %s files from %s" % (len(ncfiles), date))
+        cmd = "ncrcat "+" ".join(ncfiles)+" "+os.path.join(out_path, out_file)
+        subprocess.call([cmd], shell=True)
