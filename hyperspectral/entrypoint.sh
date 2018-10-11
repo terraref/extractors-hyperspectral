70c42bbf242053167eabf249fa691cf0c64123e4
Max Burnette <mburnet2@illinois.com>
2018-10-11 07:43:58 -0500

    nco install updates

--- hyperspectral/entrypoint.sh
+++ hyperspectral/entrypoint.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
-
 set -e
 
-# If RabbitMQ URI is not set, use the default credentials; while doing so,
-# handle the linking scenario, where RABBITMQ_PORT_5672 is set.
+# rabbitmq
 if [ "$RABBITMQ_URI" == "" ]; then
-    if [ -n $RABBITMQ_PORT_5672 ]; then
-        RABBITMQ_URI="amqp://guest:guest@${RABBITMQ_PORT_5672_TCP_ADDR}:${RABBITMQ_PORT_5672_TCP_PORT}/%2F"
-    else
-        RABBITMQ_URI="amqp://guest:guest@localhost:5672/%2F"
+    # configure RABBITMQ_URI if started using docker-compose or --link flag
+    if [ -n "$RABBITMQ_PORT_5672_TCP_ADDR" ]; then
+        RABBITMQ_URI="amqp://guest:guest@${RABBITMQ_PORT_5672_TCP_ADDR}:${RABBITMQ_PORT_5672_TCP_PORT}/${RABBITMQ_VHOST}"
     fi
+
+    # configure RABBITMQ_URI if rabbitmq is up for kubernetes
+    # TODO needs implementation maybe from NDS people
 fi
 
-. pyenv/bin/activate
+# start server if asked
+if [ "$1" = 'extractor' ]; then
+    cd /home/extractor
 
-printf "exec %s \n\n" "$@"
+    if [ "$RABBITMQ_PORT_5672_TCP_ADDR" != "" ]; then
+        # start extractor after rabbitmq is up
+        for i in `seq 1 10`; do
+            if nc -z $RABBITMQ_PORT_5672_TCP_ADDR $RABBITMQ_PORT_5672_TCP_PORT ; then
+                exec python ${MAIN_SCRIPT}
+            fi
+            sleep 1
+        done
+    fi
+
+    # just launch extractor and see what happens
+    exec python ${MAIN_SCRIPT}
+fi
+
 exec "$@"
