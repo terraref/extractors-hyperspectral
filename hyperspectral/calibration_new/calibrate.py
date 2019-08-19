70b0112086b3aeaebe1f70d235ed06d334af22a0
Max Burnette <mburnet2@illinois.com>
2019-08-19 10:19:37 -0500

    update calibrate code to include output

--- hyperspectral/calibration_new/calibrate.py
+++ hyperspectral/calibration_new/calibrate.py
@@ -1,9 +1,9 @@
 """
 *** Hyperspectral Image Calibration ***
-This module will process the raw data file and export ENVI files with variables 
+This module will process the raw data file and export ENVI files with variables
 stored its hdr file. (This is an initial version, more improvements are coming up. )
 
-input:   1) raw VNIR data; 
+input:   1) raw VNIR data;
          2) spectral data from downwelling irradiance sensor
 
 output: the reflectance image
@@ -15,7 +15,11 @@ import json
 import numpy as np
 import os
 import spectral.io.envi as envi
+from netCDF4 import Dataset
 
+
+raw_root = "/home/extractor/sites/ua-mac/raw_data"
+# raw_root = "/home/extractor/hs_calib"
 
 # extract spectral profiles from environmentlogger.json
 def irradiance_time_extractor(envlog_file):
@@ -34,47 +38,111 @@ def irradiance_time_extractor(envlog_file):
     spectra = np.zeros((num_readings, num_bands))
     times = []
     for idx in range(num_readings):
-            # read time stamp
-            time_current = js["environment_sensor_readings"][idx]["timestamp"]
-            C = time_current.replace("."," ").replace("-"," ").replace(":","")
-            ArrayTime=C.split(" ")
-            time_current_r = int(ArrayTime[3])
-            times.append(time_current_r)
-            
-            # read spectrum from irridiance sensors
-            if "spectrometers" in js["environment_sensor_readings"][idx]:
-                spectrum = js["environment_sensor_readings"][idx]["spectrometers"]["FLAME-T"]["spectrum"]
-            else:
-                spectrum = js["environment_sensor_readings"][idx]["spectrometer"]["spectrum"]
-            spectra[idx,:] = spectrum
-    
+        # read time stamp
+        time_current = js["environment_sensor_readings"][idx]["timestamp"]
+        C = time_current.replace("."," ").replace("-"," ").replace(":","")
+        ArrayTime=C.split(" ")
+        time_current_r = int(ArrayTime[3])
+        times.append(time_current_r)
+
+        # read spectrum from irridiance sensors
+        if "spectrometers" in js["environment_sensor_readings"][idx]:
+            spectrum = js["environment_sensor_readings"][idx]["spectrometers"]["FLAME-T"]["spectrum"]
+        else:
+            spectrum = js["environment_sensor_readings"][idx]["spectrometer"]["spectrum"]
+        spectra[idx,:] = spectrum
+
     return times, spectra
+
+# replace rfl_img variable in netcdf with given matrix
+def update_netcdf(inp, rfl_data, subset_range=False):
+    print("Updating %s" % inp)
+
+    out = inp.replace(".nc", "_newrfl.nc")
+
+    with Dataset(inp) as src, Dataset(out, "w") as dst:
+        # copy global attributes all at once via dictionary
+        dst.setncatts(src.__dict__)
+        # copy dimensions
+        for name, dimension in src.dimensions.items():
+            dst.createDimension(name, (len(dimension) if not dimension.isunlimited() else None))
+
+        # copy all file data except for the excluded
+        for name, variable in src.variables.items():
+            if name == "Google_Map_View":
+                continue
+
+            # Create variables
+            var_dict = (src[name].__dict__)
+            if '_FillValue' in var_dict.keys():
+                x = dst.createVariable(name, variable.datatype, variable.dimensions, fill_value=var_dict['_FillValue'])
+                del var_dict['_FillValue']
+            else:
+                x = dst.createVariable(name, variable.datatype, variable.dimensions)
+
+            # Set variables to values
+            if name != "rfl_img":
+                dst[name][:] = src[name][:]
+            else:
+                if subset_range:
+                    dst[name][:679,:,:] = rfl_data
+                    # 679-955 set to NaN
+                    dst[name][679:,:,:] = np.nan
+                else:
+                    dst[name][:,:,:] = rfl_data
+
+            # copy variable attributes all at once via dictionary
+            dst[name].setncatts(var_dict)
+
+        if 'rfl_img' not in src.variables:
+            dst.createVariable("rfl_img", "f4")
+            dst.variables['rfl_img'] = rfl_data
 
 # apply calibration algorithm to the raw data
 def apply_calibration(raw_filepath):
-    print("Applying calibration to %s" % raw_filepath)
+    print("Calibrating %s" % raw_filepath)
+
     # get necessary paths from path to _raw file
-    raw_dir = os.path.dirname(input_path)
-    raw_file = os.path.basename(input_path)
+    raw_dir = os.path.dirname(raw_filepath)
+    raw_file = os.path.basename(raw_filepath)
     md_file = os.path.join(raw_dir, "%s_metadata.json" % raw_file[:-4])
     date = raw_filepath.split("/")[-3]
-    envlog_dir   = "/home/extractor/sites/ua-mac/raw_data/EnvironmentLogger/%s" % date
+    timestamp = raw_filepath.split("/")[-2]
+    envlog_dir   = os.path.join(raw_root, "EnvironmentLogger/%s" % date)
 
     # determine type of sensor and age of camera
     if raw_filepath.find("VNIR") > -1:
         if date < "2018-08-18":
             camera_type = "vnir_old"
             num_spectral_bands = 955
+            num_bands_irradiance = 1024
+            image_scanning_time   =  540
+        elif "2018-08-18" <= date < "2019-02-26":
+            # TODO: This may need special handling - vnir_middle
+            camera_type = "vnir_new"
+            num_spectral_bands = 939
+            num_bands_irradiance = 1024
+            image_scanning_time   =  210
         else:
             camera_type = "vnir_new"
             num_spectral_bands = 939
+            num_bands_irradiance = 3648
+            # it is obverved that it takes an average of 3.5 mins/scan  = 210 seconds
+            image_scanning_time   =  210
     else:
         if date < "2018-08-17":
-            camera_type = "swir_new" # TODO: does swir_old need separate calibration?
+            # swir_old does not have necessary input files
+            camera_type = "swir_new"
             num_spectral_bands = 273
+            num_bands_irradiance = 1024 #512
+            image_scanning_time   =  210
         else:
             camera_type = "swir_new"
             num_spectral_bands = 275
+            num_bands_irradiance = 1024 #512
+            image_scanning_time   =  210
+
+    print("MODE: ---------- %s ----------" % camera_type)
 
     best_matched = os.path.join("calibration_new", camera_type, 'best_matched_index.npy')
     bias = os.path.join("calibration_new", camera_type, 'bias_coeff.npy')
@@ -91,19 +159,17 @@ def apply_calibration(raw_filepath):
 
     # read EnvLog data
     print("Reading EnvLog files in %s" % envlog_dir)
-    # num_bands_irradiance = 3648 # in new irradiance sensor, there are 3648 spectral bands
     envlog_tot_time =  []
-    envlog_spectra = None
+    envlog_spectra = np.array([], dtype=np.int64).reshape(0, num_bands_irradiance)
     for ef in os.listdir(envlog_dir):
         if ef.endswith("environmentlogger.json"):
+            print(ef)
             time, spectrum = irradiance_time_extractor(os.path.join(envlog_dir, ef))
             envlog_tot_time += time
-            if envlog_spectra is None:
-                envlog_spectra = np.array([], dtype=np.int64).reshape(0, spectrum.shape[1])
+            # print("concatenating %s onto %s" % (spectrum.shape, envlog_spectra.shape))
             envlog_spectra = np.vstack([envlog_spectra, spectrum])
 
     # Find the best match time range between image time stamp and EnvLog time stamp
-    image_scanning_time   =  210 # it is obverved that it takes an average of 3.5 mins/scan  = 210 seconds
     num_irridiance_record = int(image_scanning_time/5)   # 210/5=4.2  ---->  5 seconds per record
 
     # concatenation of hour mins and seconds of the image time stamp (eg., 12-38-49 to 123849)
@@ -114,6 +180,7 @@ def apply_calibration(raw_filepath):
     image_time     = int(image_time.replace(":",""))
 
     # computer the absolute difference between
+    print("Computing mean spectrum")
     abs_diff_time = np.zeros((len(envlog_tot_time)))
     for k in range(len(envlog_tot_time)):
         abs_diff_time[k] = abs(image_time  - envlog_tot_time[k])
@@ -122,47 +189,60 @@ def apply_calibration(raw_filepath):
 
     # load pre-computed the best matched index between image and irradiance sensor spectral bands
     best_matched_index = np.load(best_matched)
+    print("mean_spec %s" % mean_spectrum.shape)
+    print("best_matched_index %s" % best_matched_index.shape)
     test_irridance     =  mean_spectrum[best_matched_index.astype(int).tolist()]
     test_irridance_re  = np.resize(test_irridance, (1, num_spectral_bands))
 
     # load and apply precomputed coefficient to convert irradiance to DN
     b = np.load(bias)
     g = np.load(gain)
-    irrad2DN = g*test_irridance_re + b
+    if camera_type == "vnir_old":
+        test_irridance_re = test_irridance_re[:,0:679]
+        img_DN = img_DN[:,:,0:679]
+    irrad2DN = (g * test_irridance_re) + b
 
     # reflectance computation
-    Ref  = (img_DN)/(irrad2DN) # Ref stands for the reflectance image
+    print("Computing reflectance: %s vs %s" % (img_DN.shape, irrad2DN.shape))
+    rfl_data  = img_DN/irrad2DN
+    rfl_data = np.rollaxis(rfl_data, 2, 0)
 
     # prepare output paths
+    print("Generating output")
     out_path = os.path.dirname(raw_filepath.replace("raw_data", "Level_1").replace("SWIR", "swir_netcdf").replace("VNIR", "vnir_netcdf"))
-    out_file = os.path.join(out_path, 'ref_%s.npy' % raw_file) # os.path.join('ref_%s.hdr' % raw_file)
     if not os.path.isdir(out_path):
         os.makedirs(out_path)
 
-    # TODO: Write to nc file
-    """from ncdump -h *.nc
-
-    variables:
-        float rfl_img(wavelength, y, x) ;
-            rfl_img:long_name = "Reflectance of image" ;
-            rfl_img:standard_name = "surface_albedo" ;
-            rfl_img:units = "1" ;
-
-
-    with Dataset(rfl_image, "w") as netCDF_handle:
-        netCDF_handle.write(Ref)
-    """
-
     # save as ENVI file (RGB bands: 392, 252, 127)
-    # envi.save_image(out_file, Ref, dtype=np.float32, interleave='bil', force = 'True', metadata=head_file)
+    #out_file = os.path.join('ref_%s.hdr' % raw_file)
+    #envi.save_image(out_file, Ref, dtype=np.float32, interleave='bil', force = 'True', metadata=head_file)
 
     # Save Ref as a .npy file
-    np.save(out_file, Ref)
+    #out_file = os.path.join(out_path, 'ref_%s.npy' % raw_file)
+    #np.save(out_file, rfl_data)
+
+    # Write to nc file
+    out_file = os.path.join(out_path, "%s_netcdf_L1_ua-mac_%s.nc" % (camera_type.split("_")[0], timestamp))
+    update_netcdf(out_file, rfl_data, camera_type=="vnir_old")
 
 
 # TODO: This will come from the extractor message
-input_path  = "/home/extractor/sites/ua-mac/raw_data/SWIR/2017-04-16/2017-04-16__11-50-46-707/c6079666-b686-4481-9a4f-0663f5f43a6a_raw"
-# "/home/extractor/sites/ua-mac/raw_data/SWIR/2018-08-17/2018-08-17__15-44-34-088/c283d6ef-9ccf-4f8f-b291-bb753e50c0ac_raw"
-# "/home/extractor/sites/ua-mac/raw_data/VNIR/2018-01-23/2018-01-23__06-59-08-955/757ceefe-afc6-4b80-b126-e7b7abf85a4d_raw"
-# "/home/extractor/sites/ua-mac/raw_data/VNIR/2018-08-18/2018-08-18__11-11-41-890/c5f4d50f-44ad-4e23-9d92-f10e62110ac7_raw"
-apply_calibration(input_path)
+input_paths = [
+    # swir_old
+    # os.path.join(raw_root, "SWIR/2017-04-16/2017-04-16__11-50-46-707/c6079666-b686-4481-9a4f-0663f5f43a6a_raw"),
+    # swir_new
+    os.path.join(raw_root, "SWIR/2018-09-22/2018-09-22__13-21-35-977/05b7ad1a-a2d7-4dfc-bcec-b1a394ec0892_raw"),
+    os.path.join(raw_root, "SWIR/2018-10-11/2018-10-11__12-11-43-420/dc60c7d5-24bc-432b-a7cb-98ac1da73154_raw"),
+    # vnir_old
+    os.path.join(raw_root, "VNIR/2017-04-15/2017-04-15__11-33-42-265/76efd15f-928a-49f7-a008-4877b8842129_raw"),
+    os.path.join(raw_root, "VNIR/2017-06-18/2017-06-18__14-34-24-390/41a0b327-83ff-4131-b1fd-5ee5254760b6_raw"),
+    # vnir_middle
+    # os.path.join(raw_root, "VNIR/2018-08-18/2018-08-18__11-11-41-890/c5f4d50f-44ad-4e23-9d92-f10e62110ac7_raw"),
+    # os.path.join(raw_root, "VNIR/2018-10-08/2018-10-08__11-41-01-365/5e39a30f-d343-405e-a140-db26dc72eb59_raw"),
+    # vnir_new
+    # os.path.join(raw_root, "VNIR/2019-06-17/2019-06-17__14-03-29-760/d51b6f4c-9246-4da8-9a6c-786bb1dc21bf_raw"),
+
+]
+
+for p in input_paths:
+    apply_calibration(p)
